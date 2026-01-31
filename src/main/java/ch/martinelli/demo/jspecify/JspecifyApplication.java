523c30878bee81f7f132cf8b51948754964dd4e5
Simon Martinelli <simon@martinelli.ch>
2026-01-31 12:46:38 +0100

    Initialize Spring Boot project with Maven Wrapper

---
+++ src/main/java/ch/martinelli/demo/jspecify/JspecifyApplication.java
@@ -0,0 +1,13 @@
+package ch.martinelli.demo.jspecify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspecifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspecifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/martinelli/demo/jspecify/JspecifyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.martinelli.demo.jspecify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspecifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
